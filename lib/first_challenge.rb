59a927db681dc95fff6503c9ec8e4b7d18415538
Amy Westcott <amy.westcott@yahoo.com>
2019-06-07 18:27:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts.collect do |person, data|
     data.each do |attribute, value|
-      contacts["Freddy Mercury"].delete_if{
+      contacts["Freddy Mercury"].delete_if{|key, value| 
 
 
   #remember to return your newly altered contacts hash!
