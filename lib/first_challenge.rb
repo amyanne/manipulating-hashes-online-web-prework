33cfb657d609cdb13df1b3a1a14e8f8a7f44d114
Amy Westcott <amy.westcott@yahoo.com>
2019-06-07 18:32:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,8 @@ def first_challenge
 
   contacts.collect do |person, data|
     data.each do |attribute, value|
-      contacts["Freddy Mercury"].delete_if{|key, value| key ="strawberry"}
-      binding.pry
+      contacts["Freddy Mercury"][:favorite_icecream_flavors]["strawberry"].delete_if{|key, value| key ="strawberry"}
+      
     end 
   end
 contacts
