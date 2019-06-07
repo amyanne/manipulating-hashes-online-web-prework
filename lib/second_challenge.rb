0584cf1b2ab357bba6b33a9271260ef6fdd1fe5b
Amy Westcott <amy.westcott@yahoo.com>
2019-06-07 18:41:52 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,7 @@ def second_challenge
 
   #code your solution here!
   groceries.values do |grocery, data|
-    data.values do |attribute, value|
+    data.each do |attribute, value|
       binding.pry
     end 
   end
